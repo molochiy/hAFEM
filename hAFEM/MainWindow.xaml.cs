4cfde8d19dbdb2eb7a26c9f529289017511d4f11
MMolochii <molochiy@gmail.com>
2017-06-14 23:41:39 +0300

    Added .gitignore, documents, Olesia's formuls

--- hAFEM/MainWindow.xaml.cs
+++ hAFEM/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ namespace hAFEM
   /// </summary>
   public partial class MainWindow
   {
-
     private List<IterationResult> _iterationResults;
     private InputData _inputData;
 
@@ -20,25 +19,6 @@ namespace hAFEM
       InitializeComponent();
       InitializeInputData();
       InitializeGraphPane();
-    }
-
-    private void InitializeInputData()
-    {
-      MuTxbx.Text = "0,0025";
-      BetaTxbx.Text = "0";
-      SigmaTxbx.Text = "1";
-      FTxbx.Text = "cos(pi*x)*cos(pi*x)+0,005*pi*pi*cos(2*pi*x)";
-      AlphaTxbx.Text = "1000";
-      EtaTxbx.Text = "10";
-      NTxbx.Text = "4";
-
-      //MuTxbx.Text = "1";
-      //BetaTxbx.Text = "100";
-      //SigmaTxbx.Text = "0";
-      //FTxbx.Text = "100";
-      //AlphaTxbx.Text = "100000";
-      //EtaTxbx.Text = "5";
-      //NTxbx.Text = "4";
     }
 
     public void InitializeGraphPane()
@@ -68,7 +48,7 @@ namespace hAFEM
 
     public void InitializeProblem()
     {
-      _inputData = new InputData(MuTxbx.Text, BetaTxbx.Text, SigmaTxbx.Text, FTxbx.Text, double.Parse(AlphaTxbx.Text), double.Parse(EtaTxbx.Text)) { N = int.Parse(NTxbx.Text) };
+      _inputData = new InputData(FixMinus(MuTxbx.Text), FixMinus(BetaTxbx.Text), FixMinus(SigmaTxbx.Text), FixMinus(FTxbx.Text), double.Parse(AlphaTxbx.Text), double.Parse(EtaTxbx.Text)) { N = int.Parse(NTxbx.Text) };
       _iterationResults = new List<IterationResult>();
       var iterationResult = new IterationResult
       {
@@ -87,76 +67,6 @@ namespace hAFEM
       iterationResult.Points.Add(new Point(prevPoint.Right, 1, prevPoint.Right + _inputData.H / 2, _inputData.H));
 
       _iterationResults.Add(iterationResult);
-    }
-
-    private void CalculateBtn_Click(object sender, RoutedEventArgs e)
-    {
-      InitializeProblem();
-      while (Calculate()) { }
-      ShowResults();
-    }
-
-    private bool Calculate()
-    {
-      var matrix = InitializeMatrix();
-      var vector = InitializeVector();
-
-      var results = Progonka(matrix, vector);
-
-      var lastIteration = _iterationResults.Last();
-      lastIteration.SetResults(results);
-
-      CalculateError();
-
-      return CheckForErrorsDeviation();
-    }
-
-    private double[,] InitializeMatrix()
-    {
-      var lastIteration = _iterationResults.Last();
-      var points = lastIteration.Points;
-
-      var n = lastIteration.ElementsNumber;
-
-      var matrix = new double[n + 1, n + 1];
-
-      //matrix[0, 0] = (_inputData.Mu(points[0].Middle) / points[0].H) * (-1 + (1.0 / 6) * Pe(points[0].H, points[0].Middle) * (2 * Sh(points[0].H, points[0].Middle) + 3)) + _inputData.Alpha;
-      //matrix[0, 1] = (_inputData.Mu(points[0].Middle) / points[0].H) * (-1 + (1.0 / 6) * Pe(points[0].H, points[0].Middle) * (2 * Sh(points[0].H, points[0].Middle) - 3));
-
-      for (int i = 1; i < n; ++i)
-      {
-        matrix[i, i - 1] = (_inputData.Mu(points[i - 1].Middle) / points[i - 1].H)
-                         * (-1 + (1.0 / 6) * Pe(points[i - 1].H, points[i - 1].Middle) * (Sh(points[i - 1].H, points[i - 1].Middle) - 3));
-        matrix[i, i] = -1.0 * ((_inputData.Mu(points[i - 1].Middle) / points[i - 1].H) * (-1 + (1.0 / 6) * Pe(points[i - 1].H, points[i - 1].Middle) * (2 * Sh(points[i - 1].H, points[i - 1].Middle) + 3))
-                             + (_inputData.Mu(points[i].Middle) / points[i].H) * (-1 + (1.0 / 6) * Pe(points[i].H, points[i].Middle) * (2 * Sh(points[i].H, points[i].Middle) - 3)));
-        matrix[i, i + 1] = (_inputData.Mu(points[i].Middle) / points[i].H) * (-1 + (1.0 / 6) * Pe(points[i].H, points[i].Middle) * (Sh(points[i].H, points[i].Middle) + 3));
-      }
-
-      matrix[n, n - 1] = (_inputData.Mu(points[n - 1].Middle) / points[n - 1].H) * (-1 + (1.0 / 6) * Pe(points[n - 1].H, points[n - 1].Middle) * (2 * Sh(points[n - 1].H, points[n - 1].Middle) - 3));
-      matrix[n, n] = (_inputData.Mu(points[n - 1].Middle) / points[n - 1].H) * (-1 + (1.0 / 6) * Pe(points[n - 1].H, points[n - 1].Middle) * (2 * Sh(points[n - 1].H, points[n - 1].Middle) + 3)) + _inputData.Alpha;
-
-      return matrix;
-    }
-
-    private double[] InitializeVector()
-    {
-      var lastIteration = _iterationResults.Last();
-      var points = lastIteration.Points;
-
-      var n = lastIteration.ElementsNumber;
-
-      var vector = new double[n + 1];
-
-      vector[0] = 0;
-
-      for (int i = 1; i < n; ++i)
-      {
-        vector[i] = 0.5 * (points[i - 1].H * _inputData.F(points[i - 1].Middle) + points[i].H * _inputData.F(points[i].Middle));
-      }
-
-      vector[n] = 0.5 * points[n - 1].H * _inputData.F(points[n - 1].Middle);
-
-      return vector;
     }
 
     public double Pe(double h, double x)
@@ -207,21 +117,33 @@ namespace hAFEM
       double errorNormSquare = 0.0;
       double uNormSquare = 0.0;
 
-
-      // TODO: ERROR NORM???
       for (var i = 0; i < n; i++)
       {
-        var m = points[i].H * points[i].H * points[i].H / _inputData.Mu(points[i].Middle);
-        var b = _inputData.F(points[i].Middle) - _inputData.Beta(points[i].Middle) * points[i].MiddleResultDeriv - _inputData.Sigma(points[i].Middle) * points[i].MiddleResult;
-        var d = 10 + Pe(points[i].H, points[i].Middle) * Sh(points[i].H, points[i].Middle);
-        var errorNorm = 1.25 * (points[i].H * points[i].H / _inputData.Mu(points[i].Middle)) * b / d;
+        // h^3 / mu
+        var multiplier1 = Math.Pow(points[i].H, 3) / _inputData.Mu(points[i].Middle);
 
-        points[i].ErrorNorm = (m * b * b) / d;//Math.Abs(errorNorm);
+        // f - beta * q` - sigma * q
+        var multiplier2 = Math.Pow(
+          _inputData.F(points[i].Middle)
+          - _inputData.Beta(points[i].Middle)
+            * points[i].MiddleResultDeriv
+          - _inputData.Sigma(points[i].Middle)
+            * points[i].MiddleResult,
+          2.0);
 
-        errorNormSquare += (m * b * b) / d;
+        // 10 + Pe * Sh
+        var multiplier3 = 1.0
+          / (10
+            + Pe(points[i].H, points[i].Middle)
+              * Sh(points[i].H, points[i].Middle));
 
-        //uNormV2_i
-        uNormSquare += points[i].H * points[i].MiddleResultDeriv * points[i].MiddleResultDeriv;
+        var errorNormSquareI = multiplier1 * multiplier2 * multiplier3;
+
+        points[i].ErrorNorm = Math.Sqrt(5.0 / 6.0 * errorNormSquareI);
+
+        errorNormSquare += errorNormSquareI;
+
+        uNormSquare += points[i].H * Math.Pow(points[i].MiddleResultDeriv, 2);
       }
 
       errorNormSquare *= 5.0 / 6;
@@ -229,9 +151,11 @@ namespace hAFEM
       lastIteration.ErrorNormSquare = errorNormSquare;
       lastIteration.UNormSquare = uNormSquare;
 
+      var errorCoef = Math.Sqrt(n / (errorNormSquare + uNormSquare)) * 100;
+
       for (var i = 0; i < n; i++)
       {
-        points[i].Error = Math.Sqrt(n * points[i].ErrorNorm / (errorNormSquare + uNormSquare)) * 100;
+        points[i].Error = points[i].ErrorNorm * errorCoef;
       }
     }
 
@@ -275,6 +199,146 @@ namespace hAFEM
       FillIterationResultDataGrid();
       DrawGraphs(0);
       FillDataGrids(0);
+    }
+
+    private void InitializeInputData()
+    {
+      MuTxbx.Text = "0,0025";
+      BetaTxbx.Text = "0";
+      SigmaTxbx.Text = "1";
+      FTxbx.Text = "cos(pi*x)*cos(pi*x)+0,005*pi*pi*cos(2*pi*x)";
+      AlphaTxbx.Text = "1000";
+      EtaTxbx.Text = "10";
+      NTxbx.Text = "4";
+
+      //MuTxbx.Text = "1";
+      //BetaTxbx.Text = "100";
+      //SigmaTxbx.Text = "0";
+      //FTxbx.Text = "100";
+      //AlphaTxbx.Text = "100000";
+      //EtaTxbx.Text = "5";
+      //NTxbx.Text = "4";
+    }
+
+    private void CalculateBtn_Click(object sender, RoutedEventArgs e)
+    {
+      InitializeProblem();
+      while (Calculate()) { }
+      ShowResults();
+    }
+
+    private bool Calculate()
+    {
+      var matrix = InitializeMatrix();
+      var vector = InitializeVector();
+
+      var results = Progonka(matrix, vector);
+
+      var lastIteration = _iterationResults.Last();
+      lastIteration.SetResults(results);
+
+      CalculateError();
+
+      return CheckForErrorsDeviation();
+    }
+
+    private double[,] InitializeMatrix()
+    {
+      var lastIteration = _iterationResults.Last();
+      var points = lastIteration.Points;
+
+      var n = lastIteration.ElementsNumber;
+
+      var matrix = new double[n + 1, n + 1];
+
+      for (int i = 1; i < n; ++i)
+      {
+        // Olesia's formuls
+        matrix[i, i - 1] = (_inputData.Mu(points[i - 1].Middle) / points[i - 1].H)
+          * (-1
+            - Pe(points[i - 1].H, points[i - 1].Middle) / 2.0
+            + Sh(points[i - 1].H, points[i - 1].Middle) / 6.0);
+
+        matrix[i, i] = _inputData.Mu(points[i - 1].Middle) / points[i - 1].H
+          * (1
+            + Pe(points[i - 1].H, points[i - 1].Middle) / 2.0
+            + Sh(points[i - 1].H, points[i - 1].Middle) / 3.0)
+
+          + _inputData.Mu(points[i].Middle) / points[i].H
+          * (1
+            - Pe(points[i].H, points[i].Middle) / 2.0
+            + Sh(points[i].H, points[i].Middle) / 3.0);
+
+        matrix[i, i + 1] = _inputData.Mu(points[i].Middle) / points[i].H
+          * (-1
+            + Pe(points[i].H, points[i].Middle) / 2.0
+            + Sh(points[i].H, points[i].Middle) / 6.0);
+
+        // My Formuls
+        /*matrix[i, i - 1] = (_inputData.Mu(points[i - 1].Middle) / points[i - 1].H)
+          * (-1 + (1.0 / 6)
+          * Pe(points[i - 1].H, points[i - 1].Middle)
+          * (Sh(points[i - 1].H, points[i - 1].Middle) - 3));
+
+        matrix[i, i] = -1.0 * ((_inputData.Mu(points[i - 1].Middle) / points[i - 1].H)
+          * (-1 + (1.0 / 6)
+          * Pe(points[i - 1].H, points[i - 1].Middle)
+          * (2 * Sh(points[i - 1].H, points[i - 1].Middle) + 3))
+          
+          + (_inputData.Mu(points[i].Middle) / points[i].H) 
+          * (-1 + (1.0 / 6) * Pe(points[i].H, points[i].Middle) 
+          * (2 * Sh(points[i].H, points[i].Middle) - 3)));
+
+        matrix[i, i + 1] = (_inputData.Mu(points[i].Middle) / points[i].H) 
+          * (-1 + (1.0 / 6) 
+          * Pe(points[i].H, points[i].Middle) 
+          * (Sh(points[i].H, points[i].Middle) + 3));*/
+      }
+
+      // Olesia's formuls
+      matrix[n, n - 1] = _inputData.Mu(points[n - 1].Middle) / points[n - 1].H
+        * (-1
+          - Pe(points[n - 1].H, points[n - 1].Middle) / 2.0
+          + Sh(points[n - 1].H, points[n - 1].Middle) / 6.0);
+
+      matrix[n, n] = _inputData.Mu(points[n - 1].Middle) / points[n - 1].H
+        * (1
+          + Pe(points[n - 1].H, points[n - 1].Middle) / 2.0
+          + Sh(points[n - 1].H, points[n - 1].Middle) / 3.0)
+        + _inputData.Alpha;
+
+      // My formuls
+      /*matrix[n, n - 1] = (_inputData.Mu(points[n - 1].Middle) / points[n - 1].H) 
+        * (-1 + (1.0 / 6) 
+        * Pe(points[n - 1].H, points[n - 1].Middle) 
+        * (2 * Sh(points[n - 1].H, points[n - 1].Middle) - 3));
+      matrix[n, n] = (_inputData.Mu(points[n - 1].Middle) / points[n - 1].H) 
+        * (-1 + (1.0 / 6) * Pe(points[n - 1].H, points[n - 1].Middle) 
+        * (2 * Sh(points[n - 1].H, points[n - 1].Middle) + 3)) 
+        + _inputData.Alpha;*/
+
+      return matrix;
+    }
+
+    private double[] InitializeVector()
+    {
+      var lastIteration = _iterationResults.Last();
+      var points = lastIteration.Points;
+
+      var n = lastIteration.ElementsNumber;
+
+      var vector = new double[n + 1];
+
+      vector[0] = 0;
+
+      for (int i = 1; i < n; ++i)
+      {
+        vector[i] = 0.5 * (points[i - 1].H * _inputData.F(points[i - 1].Middle) + points[i].H * _inputData.F(points[i].Middle));
+      }
+
+      vector[n] = 0.5 * points[n - 1].H * _inputData.F(points[n - 1].Middle);
+
+      return vector;
     }
 
     private void FillIterationResultDataGrid()
@@ -366,6 +430,11 @@ namespace hAFEM
 
       DrawGraphs(iteration.IterationNumber - 1);
       FillDataGrids(iteration.IterationNumber - 1);
+    }
+
+    private static string FixMinus(string function)
+    {
+      return function[0] == '-' ? "0" + function : function;
     }
   }
 }
